45f7c32377b1673effa68c91d3e6f1290f2d0f07
github_anand <anand@domain.com>
2023-01-03 15:21:22 +0530

    Image tag changes in all files

--- components/Product.jsx
+++ components/Product.jsx
@@ -8,7 +8,7 @@ const Product = ({ product: { image, name, slug, price } }) => {
     <div>
       <Link href={`/product/${slug.current}`}>
         <div className='product-card'>
-          <img src={urlFor(image && image[0])} width={250} height={250} className='product-image' />
+          <Image src={urlFor(image && image[0])} width={250} height={250} className='product-image' />
           <p className='product-name'>{name}</p>
           <p className='product-price'>₹{price}</p>
         </div>
--- pages/success.js
+++ pages/success.js
@@ -5,7 +5,7 @@ import { BsBagCheckFill } from 'react-icons/bs';
 import { useStateContext } from '../context/StateContext';
 import { runFireworks } from '../lib/utils';
 
-const success = () => {
+const Success = () => {
     const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
 
     useEffect(() => {
@@ -35,4 +35,4 @@ const success = () => {
     )
 }
 
-export default success
+export default Success
